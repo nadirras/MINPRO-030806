4bac5c66d1ffce9798670b8f20b55f9e1a6d44d9
nadirras <75488956+nadirras@users.noreply.github.com>
2024-05-12 13:45:01 +0700

    feat: add login and register

--- apps/api/src/controllers/user.controller.ts
+++ apps/api/src/controllers/user.controller.ts
@@ -6,149 +6,35 @@ import path from 'path';
 import fs from 'fs';
 import handlebars from 'handlebars';
 import { transporter } from '../helpers/nodemailer';
+import dotenv from 'dotenv';
 // import multer from 'multer'
+dotenv.config();
+const databaseUrl = process.env.DATABASE_URL;
+const jwtKey = process.env.KEY_JWT;
+const mailUser = process.env.MAIL_USER;
 
 const prisma = new PrismaClient();
 export class UserController {
   //create account
   async createAccount(req: Request, res: Response) {
     try {
-      const { username, email, password, usedReferralCode } = req.body;
-  
-      // Check if required fields are provided
-      if (!username || !email || !password) {
-        return res.status(400).send({
-          status: 'error',
-          message: 'Username, email, and password are required fields',
-        });
-      }
-
-  
-      // Hash the password before storing in the database
+      const { password } = req.body;
       const salt = await genSalt(10);
       const hashPassword = await hash(password, salt);
-  
-      // Generate my referral code based on username
-      const initials = username.slice(0, 3).toUpperCase();
-      const randomNum = Math.floor(Math.random() * 1000);
-      const myReferralCode = `${initials}${randomNum}`;
 
-      // Prepare user data to be saved
-      let userData = {
-        username,
-        email,
-        password: hashPassword,
-
-        usedReferralCode,
-      };
-  
-      // Check if usedReferralCode is provided
-      if (usedReferralCode) {
-        // Check if the usedReferralCode exists in the database
-        const existingReferral = await prisma.referral.findFirst({
-          where: {
-            myReferralCode: usedReferralCode,
-          },
-        });
-  
-        if (!existingReferral) {
-          return res.status(400).send({
-            status: 'error',
-            message: 'Invalid referral code',
-          });
-        }
-
-  
-        // Check if the user associated with the referral code exists
-        const existingUser = await prisma.user.findUnique({
-
-          where: {
-            id: existingReferral.userId,
-          },
-        });
-  
-        if (!existingUser) {
-          return res.status(400).send({
-            status: 'error',
-            message: 'User associated with referral code not found',
-          });
-        }
-  
-        // Add logic for Referrer (pemilik referral code)
-        // Add 10,000 points to the referrer with a 3-month expiration
-        await prisma.points.create({
-          data: {
-            user: {
-              connect: {
-                id: existingReferral.userId,
-              },
-            },
-            points: 10000,
-            expired_date: new Date(
-              new Date().getTime() + 3 * 30 * 24 * 60 * 60 * 1000
-            ), // 3 months expiration
-            point_status: 'Active',
-          },
-        });
-
-  
-        // Update userData to include discountVoucher ID
-        userData = {
-          ...userData,
-        };
-      }
-  
-      // Create new user in the database
-      const newUser = await prisma.user.create({
-
-        data: userData,
-        include: {
-          discountVoucher: true, // Include related discountVoucher in the response
-          referral: true, // Include related referral in the response
-        },
-      });
-  
-      // Create new Referral record for the user
-      const referral = await prisma.referral.create({
+      const users = await prisma.user.create({
         data: {
-          myReferralCode: myReferralCode,
-          user: {
-            connect: {
-              id: newUser.id,
-            },
-          },
+          ...req.body,
+          password: hashPassword,
         },
       });
 
-      // Generate discount coupon for the new user
-      const discountCode = `${usedReferralCode.slice(0, 3)}-${new Date().getFullYear()}-${new Date().getMonth() + 1}`.replace(/-/g, '');
-  
-      // Create DiscountCoupon Record for the new user
-      const newDiscountVoucher = await prisma.discountVoucher.create({
-        data: {
-          user: {
-            connect: {
-              id: newUser.id,
-            },
-          },
-          discountCoupon: discountCode,
-          discountPercentage: 10, // 10% discount
-          expired_date: new Date(
-            new Date().getTime() + 3 * 30 * 24 * 60 * 60 * 1000
-          ), // 3 months expiration
-          discount_status: 'Active',
-        },
-      });
-  
-      // Generate JWT token
       const payload = {
-        id: newUser.id,
+        id: users.id,
       };
-  
-      const token = sign(payload, process.env.KEY_JWT!);
-  
-      // Prepare email template
-      const link = `http://localhost:3000/verify/${token}`;
+      const token = sign(payload, jwtKey!, { expiresIn: '1h' });
+      const link = `http://localhost:3000/verifikasi/${token}`;
+
       const templatePath = path.join(
         __dirname,
         '../templates',
@@ -157,39 +43,29 @@ export class UserController {
       const templateSource = fs.readFileSync(templatePath, 'utf-8');
       const compiledTemplate = handlebars.compile(templateSource);
       const html = compiledTemplate({
-        name: newUser.username,
+        name: users.username,
         link,
       });
-  
-      // Send registration email
+
       await transporter.sendMail({
-        from: 'diahnof@gmail.com',
-        to: newUser.email,
-        subject: 'Welcome to Loket.com',
+        from: mailUser,
+        to: users.email,
+        subject: 'Welcome to EventZenith',
         html,
       });
-  
-      // Send response
+
       res.status(201).send({
         status: 'ok',
-        message: 'User registered successfully',
-
-        user: newUser,
-        referral,
-
-        discountVoucher: newDiscountVoucher, // Include discount vouchers in the response
-
+        message: 'User created!',
+        users,
       });
-    } catch (err) {
-      console.error('Failed to register account:', err);
+    } catch (error) {
       res.status(400).send({
         status: 'error',
-        message: 'Failed to register account',
+        message: error,
       });
     }
   }
-  
-
   //login account
   async loginAccount(req: Request, res: Response) {
     try {
@@ -206,7 +82,7 @@ export class UserController {
       const payload = {
         id: user.id,
       };
-      const token = sign(payload, process.env.KEY_JWT!, { expiresIn: '1d' });
+      const token = sign(payload, jwtKey!, { expiresIn: '1d' });
 
       res.status(200).send({
         status: 'OK',
@@ -217,6 +93,45 @@ export class UserController {
       res.status(400).send({
         status: 'error',
         message: err,
+      });
+    }
+  }
+
+  async keepLogin(req: Request, res: Response) {
+    try {
+      const users = await prisma.user.findFirst({
+        where: { id: req?.user?.id },
+      });
+      res.status(200).send({
+        status: 'ok',
+        users,
+      });
+    } catch (error) {
+      res.status(400).send({
+        status: 'error',
+        error,
+      });
+    }
+  }
+
+  async verifyUser(req: Request, res: Response) {
+    try {
+      await prisma.user.update({
+        data: {
+          activation: true,
+        },
+        where: {
+          id: req?.user?.id,
+        },
+      });
+      res.status(200).send({
+        status: 'ok',
+        message: 'verify account success',
+      });
+    } catch (error) {
+      res.status(400).send({
+        status: 'error',
+        error,
       });
     }
   }
--- apps/web/src/components/login-partial/Login.tsx
+++ apps/web/src/components/login-partial/Login.tsx
@@ -1,33 +1,97 @@
 'use client';
 import Link from 'next/link';
 import React from 'react';
+import { loginUser } from '@/lib/user';
+import { useAppDispatch } from '@/lib/features/hooks';
+import { setUser } from '@/lib/features/user/userSlice';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { useRouter, useSearchParams } from 'next/navigation';
+import * as yup from 'yup';
+import { createToken } from '@/app/action';
+
+const LoginSchema = yup.object().shape({
+  email: yup.string().email('invalid email').required('email required'),
+  password: yup
+    .string()
+    .min(6, 'password must be at least 6 characters')
+    .required('password required'),
+});
 
 export default function Login() {
+  const search = useSearchParams();
+  const redirect = search.get('redirect') || '/';
+  const dispatch = useAppDispatch();
+
+  const onLogin = async (data: any) => {
+    try {
+      const res = await loginUser(data);
+      dispatch(setUser(res.user));
+      createToken(res.token, redirect);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <h1 className="text-3xl font-bold p-9">Login</h1>
-        <div className="card-body">
-          <label className="input input-bordered flex items-center gap-2">
-            Name
-            <input type="text" className="grow" placeholder="Daisy" />
-          </label>
-          <label className="input input-bordered flex items-center gap-2">
-            Password
-            <input type="password" className="grow" placeholder="*****" />
-          </label>
-          <p>Forgot password?</p>
-          <p>
-            Didn't have account?{' '}
-            <span className="text-primary">
-              <Link href="/register">Register</Link>
-            </span>
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Login</button>
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      validationSchema={LoginSchema}
+      onSubmit={(values, action) => {
+        console.log(values);
+        onLogin(values);
+        action.resetForm();
+      }}
+    >
+      <Form className="h-screen flex justify-center items-center">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <h1 className="text-3xl font-bold p-9">Login</h1>
+          <div className="card-body">
+            <label className="input input-bordered flex items-center gap-2">
+              Email
+              <Field
+                name="email"
+                type="text"
+                className="grow"
+                placeholder="cetewindt@mail.com"
+              />
+              <ErrorMessage
+                name="email"
+                component={'div'}
+                className="text-sm text-red-500"
+              />
+            </label>
+            <label className="input input-bordered flex items-center gap-2">
+              Password
+              <Field
+                name="password"
+                type="password"
+                className="grow"
+                placeholder="*****"
+              />
+              <ErrorMessage
+                name="password"
+                component={'div'}
+                className="text-sm text-red-500"
+              />
+            </label>
+            <p>Forgot password?</p>
+            <p>
+              Didn't have account?{' '}
+              <span className="text-primary">
+                <Link href="/register">Register</Link>
+              </span>
+            </p>
+            <div className="card-actions justify-end">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </Form>
+    </Formik>
   );
 }
